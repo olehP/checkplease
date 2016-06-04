f13e8e31417ad6c21e3872518e124ca65ea1db64
Oleh Pylypchak <pylypchak.oleh@gmail.com>
2016-06-04 05:48:16 +0300

    added dao methods

--- src/main/java/com/angelhack/dao/TableDAO.java
+++ src/main/java/com/angelhack/dao/TableDAO.java
@@ -2,8 +2,9 @@ package com.angelhack.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.angelhack.entity.Restaurant;
 import com.angelhack.entity.Table;
 
 public interface TableDAO extends JpaRepository<Table, Integer> {
-
+	Table findByRestaurantAndNumber(Restaurant restaurant, Number number);
 }
--- src/main/java/com/angelhack/message/processor/CustomerMessageProcessor.java
+++ src/main/java/com/angelhack/message/processor/CustomerMessageProcessor.java
@@ -1,8 +1,45 @@
 package com.angelhack.message.processor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.angelhack.dao.CustomerDAO;
+import com.angelhack.dao.RestaurantDAO;
+import com.angelhack.entity.Customer;
+import com.angelhack.entity.Restaurant;
+import com.angelhack.model.incomming.MessageRecieved;
+import com.angelhack.model.incomming.Messaging;
+import com.angelhack.service.CustomerService;
+import com.angelhack.util.NumberUtil;
 
 @Service
 public class CustomerMessageProcessor {
+	@Autowired
+	private CustomerService customerService;
+	@Autowired
+	private CustomerDAO customerDAO;
+	@Autowired
+	private RestaurantDAO restaurantDAO;
 
+	public void processMessage(MessageRecieved message) {
+		Messaging messaging = message.getEntry().get(0).getMessaging().get(0);
+		if (!customerService.createIfNotExist(messaging.getSender())) {
+			Customer customer = customerDAO.findByChatId(messaging.getSender().getId());
+			if (messaging.getMessage() != null && messaging.getMessage().getText() != null) {
+				processTextMessage(messaging.getMessage().getText(), customer);
+			} else if (messaging.getPostback() != null && messaging.getPostback().getPayload() != null) {
+				// processPayload(user, messaging.getPostback().getPayload());
+			}
+		}
+	}
+
+	private void processTextMessage(String text, Customer customer) {
+		if (text.indexOf('_') > 0) {
+			Restaurant restaurant = restaurantDAO.findByCode(text.substring(0, text.lastIndexOf(' ')));
+			Integer tableNumber = NumberUtil.tryParseInteger(text.substring(text.lastIndexOf(' ')));
+			if (restaurant != null && tableNumber != null) {
+				customerService.addCustomerToTable(restaurant, customer, tableNumber);
+			}
+		}
+	}
 }
